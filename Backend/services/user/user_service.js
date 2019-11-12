198c3845cc2564871f95c78282b68cb07ae18591
unknown <narisht.dhyani@sjsu.edu>
2019-11-11 16:02:28 -0800

    narisht is commiting for testing

--- Backend/services/user/user_service.js
+++ Backend/services/user/user_service.js
@@ -1,6 +1,6 @@
 
 module.exports.getSomething = function(req, callback){
-    console.log("Inside service");
+    console.log("Inside service");//Testing
     callback(null,{
         success: true,
         msg: "Successfully fetched the user profile" ,
