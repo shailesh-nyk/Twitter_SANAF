c96af45d0d5eec21b34ef35b2ca3821c45fc505f
unknown <narisht.dhyani@sjsu.edu>
2019-11-11 16:42:17 -0800

    Testing 3

--- Backend/services/user/user_service.js
+++ Backend/services/user/user_service.js
@@ -1,6 +1,6 @@
 
 module.exports.getSomething = function(req, callback){
-    console.log("Inside service");//Testing 2
+    console.log("Inside service");//Testing 3
     callback(null,{
         success: true,
         msg: "Successfully fetched the user profile" ,
