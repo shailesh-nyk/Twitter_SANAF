f64cc32840da08ca2d78ecb720d56dfbeaffdd1b
unknown <kuradishailesh.nayak@sjsu.edu>
2019-11-12 01:31:01 -0800

    linking tweet model to user model

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: { type: String }
+    name: { type: String },
+    handle: {type: String}
 })
 
 var UserModel = mongoose.model('users', userSchema);
