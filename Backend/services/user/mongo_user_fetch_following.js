d0ea63000710015b631e06470339fa092c8f51c7
unknown <kuradishailesh.nayak@sjsu.edu>
2019-12-02 13:29:27 -0800

    merge error fixed

--- Backend/services/user/mongo_user_fetch_following.js
+++ Backend/services/user/mongo_user_fetch_following.js
@@ -19,6 +19,8 @@ module.exports.getFollowers = function (req, callback) {
             callback(null, model.followedBy);
         }
     ).lean();
+}
+
 
 module.exports.getFollowedBy = function (req, callback) {
     const  id  = req.id;
